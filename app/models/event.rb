d8272c905db346fae32bc3c1c407d66b1e13d43b
Guillaume Fourret <fourretg@gmail.com>
2018-03-05 13:57:09 +0100

    Improve queries performance

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,43 +5,63 @@ class Event < ApplicationRecord
   scope :openings, -> { where(kind: 'opening') }
   scope :appointments, -> { where(kind: 'appointment') }
 
-  def self.recurring_openings_for_weekday(weekday)
+  def self.recurring_openings
     openings
+      .select(:starts_at, :ends_at, :weekly_recurring)
       .where(weekly_recurring: true)
-      .where("strftime('%w', starts_at) = '?'", weekday)
+      .group(%i[starts_at ends_at])
   end
 
-  def self.solely_opened_for_day(date)
+  def self.solely_opened_for_week(date)
     openings
-      .where(weekly_recurring: [false, nil])
-      .where('starts_at >= ? AND ends_at <= ?', date, date + 1.day)
+      .select(:starts_at, :ends_at, :weekly_recurring)
+      .where('starts_at >= ? AND ends_at <= ?', date, date + PAGINATED_DAYS)
   end
 
-  def self.openings_for_day(date)
-    recurring_openings_for_weekday(date.wday) + solely_opened_for_day(date)
+  def self.openings_for_week(date)
+    (recurring_openings + solely_opened_for_week(date))
+      .to_a
   end
 
-  def self.appointments_for_day(date)
+  def self.appointments_for_week(date)
     appointments
-      .where(starts_at: date.beginning_of_day..date.end_of_day)
+      .select(:starts_at, :ends_at)
+      .where(starts_at: date..(date + PAGINATED_DAYS))
+      .to_a
   end
 
   def self.availabilities(requested_date_time)
+    openings = openings_for_week(requested_date_time)
+    appointments = appointments_for_week(requested_date_time)
+
     _selected_days(requested_date_time).map do |date|
-      { date: date, slots: available_slots_for_day(date) }
+      { date: date, slots: available_slots_for_day(date, openings, appointments) }
     end
   end
 
-  def self.available_slots_for_day(date)
-    opening_slots(date) - appointment_slots(date)
+  def self.available_slots_for_day(date, openings, appointments)
+    opening_slots_for_day(date, openings) - appointment_slots_for_day(date, appointments)
   end
 
-  def self.opening_slots(date)
-    openings_for_day(date).map(&:to_slots).flatten.uniq
+  def self.opening_slots_for_day(date, openings)
+    openings
+      .select do |opening|
+        opening.starts_at.to_date == date.to_date ||
+          (opening.weekly_recurring == true && opening.starts_at.wday == date.wday)
+      end
+      .map(&:to_slots)
+      .flatten
+      .uniq
   end
 
-  def self.appointment_slots(date)
-    appointments_for_day(date).map(&:to_slots).flatten.uniq
+  def self.appointment_slots_for_day(date, appointments)
+    appointments
+      .select do |appointment|
+        appointment.starts_at.to_date == date.to_date
+      end
+      .map(&:to_slots)
+      .flatten
+      .uniq
   end
 
   def to_slots
